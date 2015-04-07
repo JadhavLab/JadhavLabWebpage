d7cbcbc20830ebc38ca1dcd057a8e21aa85850ea
Ryan <NeuroRYoung@gmail.com>
2015-04-07 17:17:01 -0400

    First commit. Added first version of both pssible templates for Version 4 of Jadhav Lab webpage.

---
+++ SJ_Ver4/Template Candidate 2/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ryan Young on 3/29/15.
+ *
+ * This script file contains the javascript and jquery code specific to the general functioning of the index.html file
+ */
+
+/*$(document).read( function() {
+
+    // Fade-in of main page.
+    $("html").css("display","none");
+    $("html").fadeIn(1000);
+    $("#menu, #menu li, #menu li a").css("display","none");
+    $("#menu, #menu li, #menu li a").fadeIn(2000);
+
+});*/
